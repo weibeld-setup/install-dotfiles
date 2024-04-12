00334d35988009fc1c8da5a7c30d22b49a65f392
Daniel Weibel <danielmweibel@gmail.com>
2024-04-12 17:21:18 +0700

    Add .dotfiles.info directory with README and install
    Eventually replacing the README and install script in the gist [1].
    
    The install script has been changed as follows:
    
    - Repository (Git directory) is .dotfiles.git instead of .dotfiles
    - Existing dotfiles with the same name as repository dotfiles are
      not automatically backed up, but listed and the user is prompted
      to confirm to proceed or not
    
    Note: with the .dotfiles.info directory, no separate gist for the
    README and install script is necessary anymore. The drawback is
    that the .dotfiles.info directory will be included in the user's
    HOME directory, however, this shouldn't be a big problem, and it
    fits in well with the .dotfiles.git directory containing the Git
    repository.
    
    [1] https://gist.github.com/weibeld/869f723063811e5088708a9386bf52bf

---
+++ .dotfiles.info/README.md
@@ -0,0 +1,30 @@
+# Install: \<Resource>
+
+![macOS](https://raw.githubusercontent.com/weibeld-setup/.github/main/badge/macos.svg)
+![Linux](https://raw.githubusercontent.com/weibeld-setup/.github/main/badge/linux.svg)
+![Windows](https://raw.githubusercontent.com/weibeld-setup/.github/main/badge/windows.svg)
+![Android](https://raw.githubusercontent.com/weibeld-setup/.github/main/badge/android.svg)
+
+\<Short description of the installation>
+
+## Installation
+
+### macOS
+
+![macOS](https://raw.githubusercontent.com/weibeld-setup/.github/main/badge/macos.svg)
+
+### Linux
+
+![Linux](https://raw.githubusercontent.com/weibeld-setup/.github/main/badge/linux.svg)
+
+### Windows
+
+![Windows](https://raw.githubusercontent.com/weibeld-setup/.github/main/badge/windows.svg)
+
+### Android
+
+![Android](https://raw.githubusercontent.com/weibeld-setup/.github/main/badge/android.svg)
+
+## Notes
+
+
---
+++ .dotfiles.info/install.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Git repository directory (.git directory)
+git_dir=$HOME/.dotfiles.git
+# Workspace directory (where files are checked out)
+work_tree=$HOME
+
+echo "> Cloning repository..."
+git clone --quiet --bare https://github.com/weibeld-setup/install-dotfiles-test "$git_dir"
+
+# List files/dirs that will be replaced by checkout
+repo_files=($(git --git-dir="$git_dir" --work-tree="$work_tree" ls-tree --name-only HEAD))
+conflict_files=()
+for f in "${repo_files[@]}"; do
+  [[ -e ~/"$f" ]] && conflict_files+=("$f")
+done
+if [[ "${#conflict_files[@]}" -gt 0 ]]; then
+  echo "> The following files and directories will be overwritten:"
+  for f in "${conflict_files[@]}"; do
+    echo "  - $HOME/$f"
+  done
+  read -p "> Proceed (Y/n)? " response
+  if [[ "$response" =~ n|N ]]; then
+    echo "> Deleting cloned repository..."
+    rm -rf "$git_dir"
+    echo "> Exiting (no changes have been applied)"
+    exit
+  fi
+fi
+
+# Delete files/dirs that will be replaced (prevents dirs from merging)
+for f in "${conflict_files[@]}"; do
+  rm -rf "$f"
+done
+
+# Check out files/dirs
+# Note: if there are submodules, .gitmodules is checked out to the workspace too
+echo "> Checking out files and directories:"
+for f in "${repo_files[@]}"; do
+  echo "  - $HOME/$f"
+done
+git --git-dir="$git_dir" --work-tree="$work_tree" checkout
+
+# Check out submodules
+submodules=($(git --git-dir="$git_dir" --work-tree="$work_tree" submodule status | awk '{print $2}'))
+if [[ "${#submodules[@]}" -gt 0 ]]; then
+  echo "> Checking out submodules:" 
+  for s in "${submodules[@]}"; do
+    echo "  - $s..."
+    git --git-dir="$git_dir" --work-tree="$work_tree" submodule --quiet update --init "$s"
+  done
+fi
+
+# Omit files that are not part of the repository in 'git status' output
+git --git-dir="$git_dir" --work-tree="$work_tree" config status.showUntrackedFiles no
