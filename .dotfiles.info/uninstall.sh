9736ac423bbadcc3712d33f9727c7854146a6b64
Daniel Weibel <danielmweibel@gmail.com>
2024-04-19 16:08:10 +0700

    Fix deletion of empty directories in uninstall.sh
    Also check subdirectories of top-level directories in the repo.

--- .dotfiles.info/uninstall.sh
+++ .dotfiles.info/uninstall.sh
@@ -10,24 +10,27 @@ git_dir=$HOME/.dotfiles.git
 # Workspace directory (where files are checked out)
 work_tree=$HOME
 
-# Delete all files
+# Delete all files and submodule directories
 echo "> Deleting files..."
 git --git-dir "$git_dir" ls-files |
   while read f; do
     echo "  - $f"
-    rm "$HOME/$f"
+    # ls-files also lists submodule directories, hence 'rm -rf' must be used
+    rm -rf "$work_tree/$f"
   done
 
 # Delete directories that don't contain any files
 echo "> Deleting empty directories..."
-git --git-dir "$git_dir" ls-tree -d --format '%(path)' HEAD |
+git ls-tree -rd --format '%(objecttype) %(path)' HEAD |
+  grep '^tree ' |
+  sed 's/^tree //' |
   while read d; do
-    if [[ -z $(find "$d" -not -type d) ]]; then
+    if [[ -d "$work_tree/$d" && -z $(find "$work_tree/$d" -not -type d) ]]; then
       echo "  - $d"
-      rm -rf "$HOME/$d"
+      rm -rf "$work_tree/$d"
     fi
   done
 
 # Delete repository directory
-echo "> Deleting repository ~/.dotfiles.git"
+echo "> Deleting repository '~/.dotfiles.git'"
 rm -rf "$git_dir"
