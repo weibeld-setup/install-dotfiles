427a5c330d23e85d8eae52f9b71f48b33894f146
Daniel Weibel <danielmweibel@gmail.com>
2024-04-20 19:40:44 +0700

    Separate submodules from files in uninstall.sh

--- .dotfiles.info/uninstall.sh
+++ .dotfiles.info/uninstall.sh
@@ -8,38 +8,53 @@ set -o pipefail
 
 # Git repository directory (.git directory)
 git_dir=$HOME/.dotfiles.git
+
 # Workspace directory (where files are checked out)
 work_tree=$HOME
 
 if [[ ! -d "$git_dir" ]]; then
-  echo "Error: dotfiles repository '$git_dir' not found"
-  echo "> Are the dotfiles actually installed?"
+  echo "Error: the dotfiles repository '$git_dir' does not exist"
   exit 1  
 fi
 
-# Delete all files and submodule directories
-echo "> Deleting files..."
-git --git-dir "$git_dir" ls-files |
-  while read f; do
-    echo "    $f"
-    # ls-files also lists submodule directories, hence 'rm -rf' must be used
-    rm -rf "$work_tree/$f"
+# Delete files
+items=$(git --git-dir "$git_dir" ls-tree -r --format '%(objecttype) %(path)' HEAD | grep '^blob ' | sed 's/^blob //')
+if [[ -n "$items" ]]; then
+  echo "> Deleting files in '$work_tree'..."
+  echo "$items" | while read p; do
+    echo "    $p"
+    rm  "$work_tree/$p"
   done
+fi
 
-# Delete directories that don't contain any files
-echo "> Deleting empty directories..."
-git --git-dir "$git_dir" ls-tree -rd --format '%(objecttype) %(path)' HEAD |
-  grep '^tree ' |
-  sed 's/^tree //' |
-  while read d; do
+# Delete submodules
+items=$(git --git-dir "$git_dir" ls-tree -r --format '%(objecttype) %(path)' HEAD | grep '^commit ' | sed 's/^commit //')
+if [[ -n "$items" ]]; then
+  echo "> Deleting submodules in '$work_tree'..."
+  echo "$items" | while read p; do
+    echo "    $p/"
+    rm  -rf "$work_tree/$p"
+  done
+fi
+
+# Delete empty directories
+items=$(
+  for d in $(git --git-dir "$git_dir" ls-tree -rd --format '%(objecttype) %(path)' HEAD | grep '^tree ' | sed 's/^tree //'); do
     if [[ -d "$work_tree/$d" && -z $(find "$work_tree/$d" -not -type d) ]]; then
-      echo "    $d"
-      rm -rf "$work_tree/$d"
+      echo "$d"
     fi
   done
+)
+if [[ -n "$items" ]]; then
+  echo "> Deleting empty directories in '$work_tree'..."
+  echo "$items" | while read p; do
+    echo "    $p/"
+    rm  -rf "$work_tree/$p"
+  done
+fi
 
 # Delete repository directory
-echo "> Deleting repository '~/.dotfiles.git'"
+echo "> Deleting repository '$git_dir'"
 rm -rf "$git_dir"
 
 echo "✅ UNINSTALLATION COMPLETE"
